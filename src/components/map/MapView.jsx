0c918d397aacf801eca964877ad5e619a8f00053
jiujiu2899 <jiujiu281999@gmail.com>
2026-03-26 23:28:12 +0700

    leaftlet react 1

--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -1,8 +1,40 @@
+import { LayersControl, MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const MapView = () => {
   return (
-    <div className="flex-1">MapView</div>
-  )
-}
+    <div className="flex-1">
+      <MapContainer
+        className="h-full"
+        center={[13, 100]}
+        zoom={7}
+        scrollWheelZoom={true}
+      >
+        <LayersControl>
+          {/* 1. Basemap */}
+          <LayersControl.BaseLayer name="OSM" checked>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Esri.WorldImagery" checked>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            />
+          </LayersControl.BaseLayer>
 
-export default MapView
+          <LayersControl.Overlay name="test" checked>
+            <Marker position={[13,100]}>
+
+            </Marker>
+          </LayersControl.Overlay>
+          
+        </LayersControl>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapView;
