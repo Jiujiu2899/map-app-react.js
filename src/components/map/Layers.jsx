fa8521641fe1d31a3d77ca2b896cffdaac845ee1
jiujiu2899 <jiujiu281999@gmail.com>
2026-03-26 23:53:44 +0700

    leaftlet react 2 end

---
+++ src/components/map/Layers.jsx
@@ -0,0 +1,29 @@
+
+import { LayersControl, Marker, TileLayer } from "react-leaflet";
+
+const Layers = () => {
+  
+  return (
+    <LayersControl>
+      {/* 1. Basemap */}
+      <LayersControl.BaseLayer name="OSM" checked>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </LayersControl.BaseLayer>
+      <LayersControl.BaseLayer name="Esri.WorldImagery" checked>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+        />
+      </LayersControl.BaseLayer>
+
+      <LayersControl.Overlay name="test" checked>
+        <Marker position={[13, 100]}></Marker>
+      </LayersControl.Overlay>
+    </LayersControl>
+  );
+};
+
+export default Layers;
--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -1,37 +1,19 @@
-import { LayersControl, MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
+import { MapContainer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import Layers from "./Layers";
 
 const MapView = () => {
+  const center = [13, 100];
+
   return (
     <div className="flex-1">
       <MapContainer
         className="h-full"
-        center={[13, 100]}
+        center={center}
         zoom={7}
         scrollWheelZoom={true}
       >
-        <LayersControl>
-          {/* 1. Basemap */}
-          <LayersControl.BaseLayer name="OSM" checked>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Esri.WorldImagery" checked>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-            />
-          </LayersControl.BaseLayer>
-
-          <LayersControl.Overlay name="test" checked>
-            <Marker position={[13,100]}>
-
-            </Marker>
-          </LayersControl.Overlay>
-          
-        </LayersControl>
+        <Layers />
       </MapContainer>
     </div>
   );
